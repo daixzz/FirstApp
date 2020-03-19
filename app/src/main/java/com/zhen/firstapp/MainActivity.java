d32cea6bdb0ea61013521b7722f13fe1d0cfd20f
dxzz <1095212454@qq.com>
2020-03-19 12:14:37 +0800

    Happy birthday homework and simon d

--- app/src/main/java/com/zhen/firstapp/MainActivity.java
+++ app/src/main/java/com/zhen/firstapp/MainActivity.java
@@ -8,17 +8,39 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    TextView out;
+    EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        out=(TextView)findViewById(R.id.TextView);
+        edit=(EditText)findViewById(R.id.EditText);
+
+        Button btn=(Button)findViewById(R.id.button);
+        btn.setOnClickListener(this);
     }
+    public void onClick(View v){
+        Log.i("main","onClick msg....");
+
+        String str=edit.getText().toString();
+        int num=Integer.parseInt(str);
+        double hua=num*1.8+32;
+        out.setText(String.valueOf(hua));
+    }
+
+
+
     }
 
 
