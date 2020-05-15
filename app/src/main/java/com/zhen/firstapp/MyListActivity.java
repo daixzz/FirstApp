12e7d4d9c017b7bb62f58fa6c409e435d6a42183
dxzz <1095212454@qq.com>
2020-05-16 00:56:34 +0800

    GridView及数据项的修改

--- app/src/main/java/com/zhen/firstapp/MyListActivity.java
+++ app/src/main/java/com/zhen/firstapp/MyListActivity.java
@@ -1,13 +1,21 @@
 package com.zhen.firstapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MyListActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    List<String> data = new ArrayList<String>();
+    private String TAG="MyList";
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +23,26 @@ public class MyListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_list);
 
         ListView listView =  findViewById(R.id.mylist);
-        String data[]={"111","222","333"};
+        //init data
+        for(int i = 0 ;i<10;i++){
+            data.add("item" + i);
+        }
 
-        ListAdapter adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
         listView.setAdapter(adapter);
+        listView.setEmptyView(findViewById(R.id.nodata));
+        listView.setOnItemClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> listv, View view, int position, long id) {
+        Log.i(TAG,"OnItemClick:position="+ position);
+        Log.i(TAG,"OnItemClick:parent="+ listv);
+        adapter.remove(listv.getItemAtPosition(position));
+        adapter.notifyDataSetChanged();
 
 
     }
